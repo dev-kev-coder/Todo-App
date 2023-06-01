342134cceb9dfef871ec992d5ab3a922ec0c0562
kevdev <recodedkevdev@gmail.com>
2023-05-31 22:33:38 -0700

    added basic models for application

---
+++ todo-app/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿namespace todo_app.Models
+{
+    public class ApplicationUser
+    {
+        public int ApplicationUserID { get; set; }
+       
+        public string? UserName { get; set; }
+       
+        public string? Password { get; set; } 
+    }
+}
---
+++ todo-app/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿namespace todo_app.Models
+{
+    public class Category
+    {
+        public int? CategoryID { get; set; }
+
+        public string? CategoryName { get; set; }
+
+        public List<TaskItem>? TaskItems { get; set; }
+
+        public DateTime? DateTimeCreated { get; set; } = DateTime.Now;
+
+        public DateTime? DateTimeModified { get; set; }
+
+        public DateTime? DateTimeDeleted { get; set; }
+
+        public ApplicationUser? ApplicationUser { get; set; }
+    }
+}
